f2acae32916264a0c77df18158f973bcca624999
Felipe Cauê <felipecaue.lb@gmail.com>
2026-03-09 22:04:04 -0300

    DI

---
+++ src/19-di/go.mod
@@ -0,0 +1,8 @@
+module github.com/felipecaue-lb/pos-go-expert/src/19-di
+
+go 1.26.1
+
+require (
+	github.com/google/wire v0.7.0
+	github.com/mattn/go-sqlite3 v1.14.34
+)
---
+++ src/19-di/wire.go
@@ -0,0 +1,25 @@
+//go:build wireinject
+// +build wireinject
+
+package main
+
+import (
+	"database/sql"
+
+	"github.com/felipecaue-lb/pos-go-expert/src/19-di/product"
+	"github.com/google/wire"
+)
+
+var setRepositoryDependency = wire.NewSet(
+	product.NewProductRepository,
+	wire.Bind(new(product.ProductRepositoryInterface), new(*product.ProductRepository)),
+)
+
+func NewUseCase(db *sql.DB) *product.ProductUseCase {
+	wire.Build(
+		setRepositoryDependency,
+		product.NewProductUseCase,
+	)
+
+	return &product.ProductUseCase{}
+}
